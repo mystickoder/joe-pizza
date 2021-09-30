2fd1f709bae1f5df0c1f7f4d1eb26685feeb73db
mystickoder <89115482+mystickoder@users.noreply.github.com>
2021-09-30 15:12:33 -0400

    Add files via upload
    joe pizza

---
+++ joe-pizza.py
@@ -0,0 +1,19 @@
+pizza = ['mushrooms', 'onions','tomatoes','cheese']
+
+if "red onions" in pizza:
+    print("add red eyes")
+
+else:
+    print("no red eyes")
+
+if "mushroom" in pizza:
+    print("will this be a veggie or meat lovers")
+
+else:
+    print("no meat on this pie")
+
+if "tomatoes" in pizza:
+    print("veggie pizza coming up")
+
+print("one veggie lover pizza")
+print("Thanks for visiting Joe's Pizza")
